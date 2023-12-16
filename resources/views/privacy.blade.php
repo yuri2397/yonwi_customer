d94d2b70d14cebf80bed42aef5843b25bd00699b
yuri2397 <diawlemor97@gmail.com>
2023-12-16 16:39:59 +0000

    Add privacy route

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Politique de Confidentialité</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- Ajoutez des styles CSS ici si nécessaire -->
+</head>
+<body class="container">
+
+    <header class="navbar">
+        <h1 class="alert alert-danger">Politique de Confidentialité</h1>
+    </header>
+
+    <section>
+        <h2>Introduction</h2>
+        <p>Bienvenue sur la Politique de Confidentialité de [Nom de votre Application]. Chez [Nom de votre entreprise], nous comprenons l'importance de la protection de votre vie privée et nous nous engageons à traiter vos informations personnelles avec respect et transparence.</p>
+    
+        <p>Cette Politique de Confidentialité explique comment nous collectons, utilisons, partageons et protégeons vos données personnelles lorsque vous utilisez notre application mobile ou notre site web.</p>
+    
+        <p>En utilisant [Nom de votre Application], vous consentez aux pratiques décrites dans cette Politique de Confidentialité. Si vous avez des questions ou des préoccupations concernant la manière dont nous traitons vos données, n'hésitez pas à nous contacter à l'adresse [adresse e-mail de contact].</p>
+    </section>
+    
+
+    <section>
+        <h2>Collecte d'Informations</h2>
+        <p>Nous collectons divers types d'informations pour fournir et améliorer nos services. Ces informations peuvent inclure :</p>
+    
+        <ul>
+            <li><strong>Informations Personnelles :</strong> Lorsque vous utilisez [Nom de votre Application], il se peut que nous vous demandions de fournir certaines informations personnelles identifiables, telles que votre nom, votre adresse e-mail, ou d'autres détails qui peuvent être utilisés pour vous identifier.</li>
+            
+            <li><strong>Informations d'Utilisation :</strong> Nous recueillons des informations sur la manière dont vous utilisez notre application. Cela peut inclure des données sur les fonctionnalités que vous utilisez le plus fréquemment, les pages que vous visitez, et d'autres interactions avec notre service.</li>
+            
+            <li><strong>Données de Localisation :</strong> Pour améliorer votre expérience, nous pouvons collecter des données de localisation lorsque vous utilisez les fonctionnalités liées à la localisation de l'application.</li>
+            
+            <li><strong>Données de Dispositif :</strong> Nous pouvons également recueillir des informations sur le dispositif que vous utilisez pour accéder à notre application, y compris le modèle, le système d'exploitation, les identifiants uniques de dispositif, et d'autres informations techniques.</li>
+        </ul>
+    
+        <p>Nous utilisons ces informations pour comprendre vos besoins, personnaliser votre expérience, améliorer nos services, et vous fournir des informations pertinentes.</p>
+    </section>
+    
+
+    <!-- Ajoutez des sections pour chaque point mentionné précédemment -->
+
+    <section>
+        <h2>Coordonnées</h2>
+        <p>Pour toute question ou préoccupation concernant notre Politique de Confidentialité, n'hésitez pas à nous contacter :</p>
+    
+        <p><strong>Adresse e-mail :</strong> <a href="mailto:contact@digita.sn">contact@digita.sn</a></p>
+    
+        <p>Nous nous efforcerons de répondre à toutes les questions dans les plus brefs délais et de traiter toute préoccupation de manière sérieuse et efficace.</p>
+    </section>
+    
+
+    <section>
+        <h2>Date d'Entrée en Vigueur</h2>
+        <p>Cette Politique de Confidentialité entre en vigueur à partir du 10 Decembre 2023.</p>
+    
+        <p>Toute modification ultérieure de cette politique sera publiée sur cette page avec une nouvelle date d'entrée en vigueur.</p>
+    
+        <p>Nous vous encourageons à consulter régulièrement notre Politique de Confidentialité pour rester informé sur la manière dont nous protégeons vos données.</p>
+    </section>
+    
+
+    <section>
+        <h2>Révisions de la Politique de Confidentialité</h2>
+        <p>Nous nous réservons le droit de mettre à jour ou de modifier cette Politique de Confidentialité à tout moment. Les modifications prendront effet dès leur publication sur cette page.</p>
+    
+        <p>En continuant à utiliser Yonwi après toute modification de cette Politique de Confidentialité, vous consentez à ces modifications.</p>
+    
+        <p>Nous nous engageons à vous informer de toute modification importante par le biais de notifications sur notre site web ou par d'autres moyens de communication.</p>
+    
+        <p>Nous vous encourageons à consulter régulièrement cette page pour rester informé sur la manière dont nous protégeons vos données.</p>
+    </section>
+    
+
+    <!-- Ajoutez des éléments supplémentaires au besoin -->
+
+    <footer>
+        <p>Merci d'avoir lu notre politique de confidentialité.</p>
+    </footer>
+
+</body>
+</html>
